f318d5c4d530bff5e88db37209273e3f78fc5dc6
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-08-19 22:19:29 -0300

    blank game added

---
+++ others/ruby_blanks.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+puts "-------------------"
+puts "| * Ruby Blanks * |"
+puts "-------------------"
+puts "I want a verb, please"
+verb = gets.chomp
+puts "now, an adjective"
+adjective1 = gets.chomp
+puts "i need another one"
+adjective2 = gets.chomp
+puts "great! thank you! as last, I need a noun"
+noun = gets.chomp
+
+puts "Eu decidi #{verb} uma festa #{adjective1} pra minha #{adjective2} #{noun}"
