a8f956b908b95fb268355e44b9924d9109019ba5
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-09-01 00:48:20 -0300

    method access

--- project/classes/person.rb
+++ project/classes/person.rb
@@ -1,7 +1,22 @@
 class Person
 
+	attr_accessor :first_name, :last_name
+
 	def say_hello
 		'Hello!'
 	end
-	
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
+
+	def first_name_initial_and_last_name
+		"#{get_initial(first_name)} #{last_name}"
+	end
+
+private
+
+	def get_initial(name)
+		name.chars.first + '.'
+	end
 end
--- project/init.rb
+++ project/init.rb
@@ -3,9 +3,13 @@
 require_relative 'classes/person'
 require_relative 'classes/animal'
 
-# person = Person.new
-# puts person.say_hello
+person = Person.new
+person.first_name = 'Bob'
+person.last_name = 'Smith'
+puts person.full_name
+puts person.first_name_initial_and_last_name
 
+puts '--------'
 pig = Animal.new
 pig.noise = 'oink'
 puts pig.noise
