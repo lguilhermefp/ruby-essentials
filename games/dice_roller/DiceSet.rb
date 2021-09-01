2050c9b5cfaf487f863de962df9ed394e8c5adb5
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-09-01 01:27:39 -0300

    loop added

--- games/dice_roller/DiceSet.rb
+++ games/dice_roller/DiceSet.rb
@@ -7,17 +7,19 @@ class DiceSet
 		@dice2 = 1 + rand(6) || options[:dice2]
 	end
 
-	def set_dice(value1, value2)
-		@dice1 = value1
-		@dice2 = value2
-	end
-
 	def roll
 		set_dice(1 + rand(6), 1 + rand(6))
 	end
 
 	def display
 		puts "#{@dice1}, #{@dice2}"
+	end
+
+private
+
+	def set_dice(value1, value2)
+		@dice1 = value1
+		@dice2 = value2
 	end
 
 end
--- games/dice_roller/main.rb
+++ games/dice_roller/main.rb
@@ -1,6 +1,22 @@
 #!usr/bin/env ruby
 require_relative 'DiceSet'
 
+puts 'this is dice roller'
 myDiceSet = DiceSet.new
-
 myDiceSet.display
+
+loop do
+	puts 'type r to roll and q to quit'
+	option = gets.chomp
+	case option
+	when 'r'
+		myDiceSet.roll
+		myDiceSet.display
+	when 'q'
+		exit
+	else
+		puts 'option not recognized'
+	end
+end
+
+
